5cb1a58a89235c0e637434935f9603773481361b
AQ <aq@okaq.com>
2020-05-27 01:51:17 +0000

    Data set geometry

--- qiko/paru.js
+++ qiko/paru.js
@@ -58,10 +58,82 @@ const loop {
 	}
 };
 
+// vector data processor
+const geo = {
+	init() {
+		// symbols, mohr, th1n, deco, shape
+		// geo.a = geo.proc(th1n);
+		// geo.b = geo.line();
+	},
+	proc(data) {
+		// threejs vectors
+		let a = {};
+		// keys
+		let b = [];
+		for (const k0 in data) {
+			a[k0] = geo.vec(data[k0]);
+			b.push(k0);
+		}
+		return [a,b];
+	},
+	vec(d0) {
+		let r0 = [];
+		for (let i = 0; i < d0.length; i++) {
+			// triple depth line segments points array
+			let d1 = d0[i];
+			let d2 = d1[0];
+			let d3 = d1[1];
+			let x0 = d2[0];
+			let y0 = 1024 - d2[1];
+			let x1 = d3[0];
+			let y1 = 1024 - d3[1];
+			let v0 = new THREE.Vector3(x0,y0,0);
+			let v1 = new THREE.Vector3(x1,y1,0);
+			r0.push([v0,v1]);
+		}
+		return r0;
+	},
+	line() {
+		let b = {};
+		// material
+		b.a = {};
+		// geometry
+		b.b = {};
+		// line
+		b.c = {};
+		for (const k0 in geo.a[0]) {
+			b.a[k0] = [];
+			b.b[k0] = [];
+			b.c[k0] = [];
+			let p0 = geo.a[0][k0];
+			let r0 = Math.random();
+			let g0 = Math.random();
+			let b0 = Math.random();
+			let c0 = new THREE.Color(r0,g0,b0);	
+			for (let i = 0; i < p0.length; i++) {
+				// let r0 = Math.random();
+				// let g0 = Math.random();
+				// let b0 = Math.random();
+				// let c0 = new THREE.Color(r0,g0,b0);
+				b.a[k0][i] = new THREE.LineBasicMaterial({color:c0});
+				// b.a[k0][i] = new THREE.LineBasicMaterial({color:0x00ff00});
+				b.b[k0][i] = new THREE.BufferGeometry().setFromPoints(p0[i]);
+				b.c[k0][i] = new THREE.Line(b.b[k0][i], b.a[k0][i]);
+			}
+		}
+		return b;
+	},
+	rand() {
+		let f0 = (Math.random() * geo.a[1].length) >>> 0;
+		let k0 = geo.a[1][f0];
+		return k0;
+	}
+};
+
 // main
 (function() {
 	console.log("starting routine");
-	subs = [dom,sce];
+	subs = [dom,sce,geo];
 	subs.forEach(el => el.init());
 	window.setTimeout(e => loop.seq(e), 1000);
 })();
