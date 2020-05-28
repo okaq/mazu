54f4efb8597efb82e6b9d3b64f86716effed97c1
AQ <aq@okaq.com>
2020-05-28 23:42:51 +0000

    Symbol json data added

--- qiko/stitch.go
+++ qiko/stitch.go
@@ -13,7 +13,9 @@ import (
 )
 
 const (
-	OUT = "qola.json"
+	// OUT = "qola.json"
+	// OUT = "symbol.json"
+	KEY = "symbol"
 )
 
 var (
@@ -25,7 +27,9 @@ var (
 func open() {
 	// open output file for writing
 	var err error
-	O, err = os.Create(OUT)
+	f0 := fmt.Sprintf("%s.json", KEY)
+	// O, err = os.Create(OUT)
+	O, err = os.Create(f0)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -50,7 +54,9 @@ func dir() {
 func pat() {
 	// write json output
 	var err error
-	_, err = W.WriteString("const qola = {};\n")
+	k0 := fmt.Sprintf("const %s = {};\n", KEY)
+	_, err = W.WriteString(k0)
+	// _, err = W.WriteString("const qola = {};\n")
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -65,7 +71,8 @@ func pat() {
 		s1 := string(b0)
 		s2 := strings.Split(s0, ".")
 		if s2[1] != "txt" { continue }
-		s3 := fmt.Sprintf("qola.%s = %s;\n", s2[0], s1)
+		// s3 := fmt.Sprintf("qola.%s = %s;\n", s2[0], s1)
+		s3 := fmt.Sprintf("%s.%s = %s;\n", KEY, s2[0], s1)
 		_, err = W.WriteString(s3)
 		if err != nil {
 			fmt.Println(err)
