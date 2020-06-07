69f079b5e6a7fe8d30bfc462669665674c11edc8
AQ <aq@okaq.com>
2020-06-07 12:49:11 +0000

    Main routine and subsystems initialization

--- qiko/oron.js
+++ qiko/oron.js
@@ -1,4 +1,35 @@
 // oron shapes
 console.log(new Date().toString());
 
+// th1n font
+
+// scene
+const sce = {
+	init() {
+		sce.a = {};
+	}
+};
+
+// DOM
+const dom = {
+	init() {
+		
+	}
+};
+
+// anim
+const loop = {
+	seq() {
+		console.log("start sequence");
+	}
+};
+
+// main
+(function() {
+	console.log("gring oron");
+	subs = [dom,sce];
+	subs.forEach(el => el.init());
+	window.setTimeout(e => loop.seq(e), 1000);
+})();
+
 
