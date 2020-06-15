f2695495891f721ff8326c96c81889ed9292c2fe
AQ <aq@okaq.com>
2020-06-15 21:53:33 +0000

    Main anonymous

--- 3dtd/nano/game.js
+++ 3dtd/nano/game.js
@@ -4,4 +4,16 @@
 console.log("start 3dtd");
 console.log(new Date().toString());
 
+// game
+const game = {
+	enter() {
+		// create logical screen for intro simulator
+		// render intro
+	}
+};
 
+// main
+(function() {
+	console.log("main start");
+	game.enter();
+})();
