e7cfe10ef9d9842251b132e5f32a42331e5d3fe4
AQ <aq@okaq.com>
2020-06-17 13:37:30 +0000

    Scene, DOM, cam, render initialization

--- 3dtd/nano/game.js
+++ 3dtd/nano/game.js
@@ -9,6 +9,28 @@ const game = {
 	enter() {
 		// create logical screen for intro simulator
 		// render intro
+		sce.init();
+	}
+};
+
+// scene
+const sce = {
+	init() {
+		// dom
+		sce.a = {};
+		sce.a.r = [1920,1080,0,0];
+		sce.a.c = dom.canvas(sce.a.r);
+		dom.canvas(sce.a.r);
+		// scene
+		sce.b = new THREE.Scene();
+		// cam
+		sce.c = new THREE.OrthographicCamera(0,1920,1080,0,-100,100);
+		// renderer
+		sce.d = new THREE.WebGLRenderer({canvas:sce.a.c.can});
+		sce.d.setClearColor(0x000000);
+		sce.d.clearColor();
+
+		console.log(sce);
 	}
 };
 
