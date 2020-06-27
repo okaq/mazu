b06d6a877c0683e085022f88218316c4ca858bee
AQ <aq@okaq.com>
2020-06-27 19:37:24 +0000

    Web serve

---
+++ xh2o/xh2o.go
@@ -0,0 +1,45 @@
+// okaq xh2o web
+// AQ <aq@okaq.com>
+// 2020-06-28
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"math/rand"
+	"time"
+)
+
+const (
+	INDEX = "xh2o.html"
+)
+
+var (
+	R *rand.Rand
+)
+
+func rando() {
+	t0 := time.Now().UnixNano()
+	s0 := rand.NewSource(t0)
+	R = rand.New(s0)
+	fmt.Printf("random: %f\n", R.float32())
+}
+
+func motd() {
+	fmt.Println("web serve on $IP:8080")
+	fmt.Println(time.Now().String())
+}
+
+func XHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r)
+	http.ServeFile(w,r,INDEX)
+}
+
+func main() {
+	rando()
+	motd()
+	http.HandleFunc("/", XHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+
