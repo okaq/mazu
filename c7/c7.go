470a2188559b256a7deaea574e241a50f5d7090e
AQ <aq@okaq.com>
2020-07-08 23:22:36 +0000

    C7 web serve

---
+++ c7/c7.go
@@ -0,0 +1,56 @@
+// okaq c7 web
+// AQ <aq@okaq.com>
+// 2020-07-08
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"math/rand"
+	"time"
+)
+
+const (
+	INDEX = "c7.html"
+	NANO = "nano/"
+)
+
+var (
+	R *rand.Rand
+	C map[string]string
+)
+
+func rando() {
+	t0 := time.Now().UnixNano()
+	s0 := rand.NewSource(t0)
+	R = rand.New(s0)
+	fmt.Printf("random: %f\n", R.Float32())
+}
+
+func motd() {
+	fmt.Println("web serve on $IP:8080")
+	fmt.Println(time.Now().String())
+}
+
+func XHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r)
+	http.ServeFile(w,r,INDEX)
+}
+
+func cache() {
+	C = make(map[string]string)
+	// atomic counter for visitor count
+	// player zero
+	// pretty print cache state response
+}
+
+func main() {
+	rando()
+	motd()
+	cache()
+	http.HandleFunc("/", XHandler)
+	http.Handle("/nano/", http.StripPrefix("/nano/", http.FileServer(http.Dir(NANO))))
+	http.ListenAndServe(":8080", nil)
+}
+
+
